eb30bcaabcdf83777b8f14e84872aa378f76a4b2
Nithya MS <nithyanithus1997@gmail.com>
2016-06-04 12:42:09 +0530

    fitth commit

--- register/urls.py
+++ register/urls.py
@@ -8,6 +8,6 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
-    url(r'^user/success/', TemplateView.as_view(template_name='page.html'),
-        name='page')
+    url(r'^user/$', UserRegistrationView.as_view(), name='login'),
+    url(r'^user/success/', TemplateView.as_view(template_name='page.html'),name='page'),
 ]
